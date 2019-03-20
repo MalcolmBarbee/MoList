6f40893983d7b42393c599469fed23e4e8c677d4
Malcolm Barbee <MalcolmBarbee@pursuit.com>
2019-03-20 18:52:19 -0400

    added functions to alter state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props) 
+
+    
+    this.state = {
+      data: '',
+      dataList: []
+
+    }
+     
+  }
+
+  enterData = (e) => {
+    let data = e.target.value
+    this.setState({data})
+   
+  }
+
+  submitData = () => {
+    let {data, dataList} = this.state
+    dataList.push(data)
+    this.setState({dataList: dataList, data: ''})
+  }
+
+
+  // submitAndClear = () => {
+  //   let {data, dataList} = this.state
+  //   dataList increases
+  //   clear input
+  //   this.setState({
+  //     data: ''
+  //   })
+  // }
+
   render() {
+    console.log(this.state.data)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+      <h1>Hello world</h1>
+      
+      <input type="text" onChange={this.enterData}/><button onClick={this.submitData}> Submit </button>
+      <div>{this.state.dataList.map((data, i) => (
+         <li key ={i}>{data}</li>
+    ))}
+    </div>
+      
+      </>
     );
   }
 }
